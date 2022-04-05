bc319cb614b571a65d2d6a901310f8f2f5473c82
xelAraY <alexey.yarohovich18@gmail.com>
2022-04-05 21:10:09 +0300

    Добавьте файлы проекта.

---
+++ TI_Lab3/El_Gamal.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace TI_Lab3
+{
+    public static class El_Gamal
+    {
+        public static int FindGCD(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                    a -= b;
+                else
+                    b -= a;
+            }
+            return a;
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number == 1 || number == 2)
+                return true;
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        public static int FastExponentiation(BigInteger mod, BigInteger num, BigInteger deg)
+        {
+            BigInteger y = 1;
+            while (deg != 0)
+            {
+                while (deg % 2 == 0)
+                {
+                    deg /= 2;
+                    num = (num * num) % mod;
+                }
+                deg--;
+                y = (y * num) % mod;
+            }
+            return (int)y;
+        }
+
+        private static List<int> PrimeDivisors(int value)
+        {
+            List<int> result = new List<int>();
+            for (int i = 2; i < value; i++)
+            {
+                if (IsPrime(i) && value % i == 0)
+                {
+                    result.Add(i);
+                }
+            }
+            return result;
+        }
+
+        public static List<int> PrimitiveRoots(int value)
+        {
+            List<int> primitiveRoots = new List<int>();
+            List<int> primeDivisors = PrimeDivisors(value - 1);
+            for (int i = 2; i < value; i++)
+            {
+                int j = 0;
+                while (j < primeDivisors.Count)
+                {
+                    if (FastExponentiation(value, i, (value - 1) / primeDivisors[j]) == 1)
+                        break;
+                    j++;
+                }
+                if (j == primeDivisors.Count)
+                    primitiveRoots.Add(i);
+            }
+            return primitiveRoots;
+        }
+
+        public static short[] Encrypt(List<int> data, byte[] bytes)//g p k x
+        {
+            short[] result = new short[0];
+            int y = FastExponentiation(data[1], data[0], data[3]);
+            short a = (short)FastExponentiation(data[1], data[0], data[2]);
+            int i = 0;
+            foreach (byte by in bytes)
+            {
+                int firstNum = FastExponentiation(data[1], y, data[2]);
+                int secondNum = FastExponentiation(data[1], by, 1);
+                short b = (short)FastExponentiation(data[1], firstNum * secondNum, 1);
+                Array.Resize(ref result, result.Length+2);
+                result[i] = a;
+                result[i+1] = b;
+                i += 2;
+            }
+            return result;
+        }
+
+        public static byte[] Decrypt(List<int> data, short[] cipciphertext)//x p
+        {
+            byte[] result = new byte[0];
+            int j = 0;
+            for(int i = 0; i < cipciphertext.Length; i += 2)
+            {
+                int firstNum = FastExponentiation(data[1], cipciphertext[i+1], 1);
+                int secondNum = FastExponentiation(data[1], cipciphertext[i], data[0]*(data[1]-2));
+                byte m = (byte)FastExponentiation(data[1], firstNum*secondNum, 1);
+                Array.Resize(ref result, result.Length + 1);
+                result[j] = m;
+                j++; 
+            }
+            return result;
+        }
+    }
+}
---
+++ TI_Lab3/MainWindow.xaml.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace TI_Lab3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static string _filePath = String.Empty;
+
+        private byte[] bytes;
+
+        private short[] ciphertext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FindPrimRoots_Click(object sender, RoutedEventArgs e)
+        {
+            if(!int.TryParse(PrimeNum_TextBox.Text, out int primNum))
+            {
+                MessageBox.Show("Некорректное значение в поле с простым числом!");
+                return;
+            }
+
+            if (!El_Gamal.IsPrime(primNum))
+            {
+                MessageBox.Show("Число p должно быть простое!");
+                return;
+            }
+
+            AllPrimRoots_ListBox.Items.Clear();
+            List<int> primitiveRoots = El_Gamal.PrimitiveRoots(primNum);
+            for(int i = 0; i < primitiveRoots.Count; i++)
+            {
+                AllPrimRoots_ListBox.Items.Add(primitiveRoots[i]);
+            }
+            PrimRootsCount_TextBox.Text = AllPrimRoots_ListBox.Items.Count.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (AllPrimRoots_ListBox.SelectedItem == null)
+                return;
+            ChoosenPrimRoot_TextBox.Text = AllPrimRoots_ListBox.SelectedItem.ToString();
+        }
+
+        private void OpenFile_Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == true)
+            {
+                _filePath = ofd.FileName;
+                bytes = File.ReadAllBytes(ofd.FileName);
+                StringBuilder sb = new StringBuilder();
+                foreach(byte b in bytes)
+                {
+                    sb.Append(Convert.ToString(b, 10));
+                    sb.Append(" ");
+                }
+                PlainText_TextBox.Text = sb.ToString();
+            }
+        }
+
+        private void Encrypt_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button? btn = sender as Button;
+
+            if (PrimeNum_TextBox.Text == String.Empty)
+            {
+                MessageBox.Show("Введите простое число p!");
+                return;
+            }
+
+            if (!int.TryParse(SecretKey_TextBox.Text, out int secretKey))
+            {
+                MessageBox.Show("Секретный ключ содержит недопустимые символы!");
+                return;
+            }
+            else if(secretKey < 2 || secretKey >= int.Parse(PrimeNum_TextBox.Text) - 1)
+            {
+                MessageBox.Show("Введен неверный секретный ключ!");
+                return;
+            }
+
+            if(!int.TryParse(SecretNum_TextBox.Text, out int secretNum))
+            {
+                MessageBox.Show("Секретное число содержит недопустимые символы!");
+                return;
+            }
+            else if(secretNum < 2 || secretNum >= int.Parse(PrimeNum_TextBox.Text) - 1 || 
+                El_Gamal.FindGCD(int.Parse(PrimeNum_TextBox.Text) - 1, secretNum) != 1)
+            {
+                MessageBox.Show("Введено неверное секретное число!");
+                return;
+            }
+
+            if (AllPrimRoots_ListBox.Items.Count == 0)
+            {
+                MessageBox.Show("Необходимо найти первообразные корни");
+                return;
+            }
+
+            if (ChoosenPrimRoot_TextBox.Text == String.Empty)
+            {
+                MessageBox.Show("Выберите один из первообразных корней!");
+                return;
+            }           
+
+            if(PlainText_TextBox.Text == String.Empty)
+            {
+                MessageBox.Show("Выберите файл для шифрования!");
+                return;
+            }
+                
+            if(btn.Tag.ToString() == "Encrypt")
+            {
+                List<int> encrData = new List<int>();
+                encrData.Add(int.Parse(ChoosenPrimRoot_TextBox.Text));
+                encrData.Add(int.Parse(PrimeNum_TextBox.Text));
+                encrData.Add(int.Parse(SecretNum_TextBox.Text));
+                encrData.Add(int.Parse(SecretKey_TextBox.Text));
+
+                ciphertext = El_Gamal.Encrypt(encrData, bytes);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < ciphertext.Length; i += 2)
+                {
+                    sb.Append(Convert.ToString(ciphertext[i], 10));
+                    sb.Append(',');
+                    sb.Append(Convert.ToString(ciphertext[i + 1], 10));
+                    sb.Append(' ');
+                }
+                ResultText_TextBox.Text = sb.ToString();
+
+                string fileName = System.IO.Path.GetDirectoryName(_filePath) + '\\' + "New_" +
+                    System.IO.Path.GetFileName(_filePath);
+                var span = new Span<short>(ciphertext);
+                var byt = MemoryMarshal.AsBytes(span);
+                var byteArray = byt.ToArray();
+                File.WriteAllBytes(fileName, byteArray);
+            }
+            else
+            {
+                List<int> decrData = new List<int>();
+                decrData.Add(int.Parse(SecretKey_TextBox.Text));
+                decrData.Add(int.Parse(PrimeNum_TextBox.Text));
+                byte[] resultText = El_Gamal.Decrypt(decrData, ciphertext);
+                StringBuilder sb = new StringBuilder();
+                foreach(byte b in resultText)
+                {
+                    sb.Append(Convert.ToString(b, 10));
+                    sb.Append(' ');
+                }
+                ResultText_TextBox.Text = sb.ToString();
+
+                string fileName = System.IO.Path.GetDirectoryName(_filePath) + '\\' + "New_" +
+                    System.IO.Path.GetFileName(_filePath);
+                File.WriteAllBytes(fileName, resultText);
+            }        
+        }
+    }
+}
